5112dac3fbe6729a9983f9cb7606616632e55b84
HaCTaCNR <94096236+HaCTaCNR@users.noreply.github.com>
2024-05-19 23:57:23 +0900

    Delete nat-instance.sh

--- nat-instance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
-yum install -y iptables
-systemctl start iptavles.service
-systemctl enable iptavles.service
-iptables -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-service iptables save
